bc6400cf75b63c1f5b7e9be16d7dc09982641693
Janani Munusamy <jananimunusamy2@gamil.com>
2021-10-01 21:04:28 +0800

    2103

--- event-handler/js/events.js
+++ event-handler/js/events.js
@@ -2,7 +2,22 @@ function transformUppercase(_val){
     return _val.toUpperCase();
 }
 
-let elNamaPengguna =document.getElementById("namePengguna")
+let elNamaPengguna = document.getElementById("namaPengguna")
 elNamaPengguna.addEventListener("keyup",function(){
     elNamaPengguna.value=transformUppercase(elNamaPengguna.value)
 })
+
+function checkPassword(){
+    let elKatalaluan = document.getElementById("katalaluan")
+    let pass_val = elKatalaluan.value
+
+    if(pass_val.length > 5){
+        alert("Password check..Ok!")
+    }else{
+        alert("Password too short!")
+    }
+}
+
+//traditional DOM event handler
+let elkl = document.getElementById("katalaluan")
+elkl.onblur = checkPassword
