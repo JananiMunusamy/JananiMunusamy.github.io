e394af7ad2926b22e0983971eed5682c1261de25
Janani Munusamy <jananimunusamy2@gamil.com>
2021-10-01 20:19:04 +0800

    818

---
+++ event-handler/js/events.js
@@ -0,0 +1,8 @@
+function transformUppercase(_val){
+    return _val.toUpperCase();
+}
+
+let elNamaPengguna =document.getElementById("namePengguna")
+elNamaPengguna.addEventListener("keyup",function(){
+    elNamaPengguna.value=transformUppercase(elNamaPengguna.value)
+})
